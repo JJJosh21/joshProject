b173374064391419eae34fcda260eadd3721dd26
JJJosh21 <student10@js-c02mxrl9g083.punahou.edu>
2015-07-16 10:52:39 -1000

    Game

--- app.js
+++ app.js
@@ -1,2 +1,33 @@
-alert("Hello Cool Kid");
-// python -m SimpleHTTPServer 8000
+
+ function load(){
+    window.alert("You have eatan all my chicken");
+ }
+var Uscore = 101;
+
+var highscore = 100;
+
+var press = function () {
+    Uscore--;
+    document.getElementById('score').innerHTML = "Your Score: " + Uscore;
+    if (Uscore > highscore) {
+        document.getElementById('hiscore').innerHTML = "New Highscore: " + Uscore;
+        return;
+
+}
+};
+var Level = 1;
+
+function Cheese() {
+	if (Uscore < 1) {
+		window.alert("You have killed Scrump");
+	}
+}
+
+$('#imgone').click(function(){
+    //do w.e
+});
+
+
+
+
+
